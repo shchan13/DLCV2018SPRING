5667a2e67f741014ed239c8d2f90360c64cdd6df
r06921017 <r06921017@ntu.edu.tw>
2018-06-08 17:03:15 +0800

    remove hw5

--- hw5/hw5_p1.sh
+++
@@ -1,2 +0,0 @@
-#!./bin/bash
-python3 task1_test.py --val $1 --vl $2 --out $3
--- hw5/hw5_p2.sh
+++
@@ -1,3 +0,0 @@
-#!./bin/bash
-wget 'https://www.dropbox.com/s/i0zvr6e36phn8om/lstm_classifier_bs1.hdf5'
-python3 task2_test.py --val $1 --vl $2 --out $3
